9addf656aa320c8faaa6fd1e84f580fb477defa3
Ivan Humeniuk <ivan.humeniuk@tacanslabs.com>
2024-07-25 13:39:33 +0300

    fix: remove target

---
+++ api/target/debug/deps/api-e6203b27d36e8ba8.d
@@ -0,0 +1,8 @@
+/home/ivan/Dev/disperse_collect/api/target/debug/deps/libapi-e6203b27d36e8ba8.rmeta: src/main.rs src/api.rs src/contract.rs src/../../Contract/build/Contract.abi
+
+/home/ivan/Dev/disperse_collect/api/target/debug/deps/api-e6203b27d36e8ba8.d: src/main.rs src/api.rs src/contract.rs src/../../Contract/build/Contract.abi
+
+src/main.rs:
+src/api.rs:
+src/contract.rs:
+src/../../Contract/build/Contract.abi:
